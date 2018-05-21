b30118e9bd4cc410436c01a28352eaffa3c07bdf
SEB <sebbanholmer@gmail.com>
2018-05-21 09:41:11 +0200

    commit

---
+++ Utspring/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Utspringet</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+</head>
+<body>
+
+	<h1 id="clock">00:00:00</h1>
+
+</body>
+</html>
